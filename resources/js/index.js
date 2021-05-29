ad2430e5f4af5db8ae4c994a2edf0e6651aa6d72
haoho98 <honhathao062@gmail.com>
2021-05-29 19:03:27 +0700

    first commit

---
+++ resources/js/index.js
@@ -0,0 +1,21 @@
+const acc = document.getElementsByClassName("accordion-btn");
+
+for (let i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+
+    if (i / 2 === 0) {
+        acc[i].style.borderBottom = 'none'
+    }
+}
